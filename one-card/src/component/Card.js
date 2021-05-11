483c442db54e365a17c39ee612aaf576e12652d2
Ata-92 <atakal92@gmail.com>
2021-05-11 14:40:18 +0300

    Complete Card.js

--- one-card/src/component/Card.js
+++ one-card/src/component/Card.js
@@ -3,8 +3,11 @@ import image from "./lorem-ipsum.png";
 
 function Card() {
   return (
-    <div className="Card">
-      <img src={image} alt="Lorem Logo" />
+    <div className="card">
+      <img className="card-img-top" src={image} alt="Lorem Logo" />
+      <h1 className="card-header">Lorem Ipsum</h1>
+      <p className="card-body">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo fuga quia ullam sequi consectetur assumenda dolore officia, possimus vitae ad.</p>
+      <a className="card-footer" href="#">VIEW MORE</a>
     </div>
   )
 }
