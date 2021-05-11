e12a14ee9ea91e90fa259b2fc62b87c80da6058b
Ata-92 <atakal92@gmail.com>
2021-05-11 14:30:19 +0300

    Create a file and style App.js

--- one-card/src/component/Card.js
+++ one-card/src/component/Card.js
@@ -1,9 +1,10 @@
 import React from "react";
+import image from "./lorem-ipsum.png";
 
 function Card() {
   return (
     <div className="Card">
-
+      <img src={image} alt="Lorem Logo" />
     </div>
   )
 }
