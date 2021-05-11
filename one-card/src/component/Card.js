07d1d9dd36a4acb769ddd9545e3cf0a9fe4baa5e
Ata-92 <atakal92@gmail.com>
2021-05-11 16:07:11 +0300

    Create scsss file and add transitions

--- one-card/src/component/Card.js
+++ one-card/src/component/Card.js
@@ -1,13 +1,16 @@
 import React from "react";
 import image from "./lorem-ipsum.png";
+import "./Card.css";
 
 function Card() {
   return (
-    <div className="card">
+    <div className="card rounded-5">
       <img className="card-img-top" src={image} alt="Lorem Logo" />
-      <h1 className="card-header">Lorem Ipsum</h1>
-      <p className="card-body">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo fuga quia ullam sequi consectetur assumenda dolore officia, possimus vitae ad.</p>
-      <a className="card-footer" href="#">VIEW MORE</a>
+      <div>
+      <h1 className="card-header mt-4 fw-bold">Lorem Ipsum</h1>
+      <p className="card-body fw-bold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo fuga quia ullam sequi consectetur assumenda dolore officia, possimus vitae ad.</p>
+      </div>
+      <button className="card-footer text-info mb-2">VIEW MORE</button>
     </div>
   )
 }
